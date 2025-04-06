4a676ce9f5f67f95cba6b0f2707aa2f7a6230014
Turbobot <turbobot@vercel.com>
2025-04-06 06:33:54 +0530

    feat(create-turbo): apply official-starter transform

--- course_selling_application/index.js
+++ course_selling_application/index.js
@@ -1,3 +1,15 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
+const { userRouter }= require('./Routes/user');
+const { courseRouter } = require('./Routes/course');
+const { adminRouter } = require("./Routes/admin")
+app.use(express.json());
+
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/course", courseRouter);
+app.use("/api/v1/admin" , adminRouter);
+    app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
 
